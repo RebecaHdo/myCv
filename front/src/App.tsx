6f9b0b872e0aeab43a848f2026293bd4c5db2bf7
unknown <rbkhb98@gmail.com>
2026-02-16 16:32:32 +0100

    theme created

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,30 @@
 
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
 import CV from "./pages/Cv";
 
-const theme = createTheme();
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#951dc4"
+    },
+    background: {
+      default: "#f6f4f8"
+    }
+  },
+  typography: {
+    h3: {
+      color: "#b919d2"
+    }
+  }
+});
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <CV />
+    </ThemeProvider>
   );
 }
 
