01dba6cdba1a2248fdf8a79859e717c2c18a8c56
unknown <rbkhb98@gmail.com>
2026-03-04 17:36:14 +0100

    add avatar

--- front/src/components/cv/Header.tsx
+++ front/src/components/cv/Header.tsx
@@ -1,18 +1,46 @@
-import { Box, Grid, Link, Typography } from "@mui/material";
+import { Avatar, Box, Grid, Link, Typography, useTheme  } from "@mui/material";
 import MailIcon from '@mui/icons-material/Mail';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import ResponsiveH6 from "./ResponsiveH6";
 
 export default function Header() {
+
+  const theme = useTheme();
+
   return (
     <Box>
-      <Typography variant="h2">
-        Rebeca Hernando Brecht
-      </Typography>
-      <Typography variant="h5" color="primary" fontWeight="bold">
-        Ingeniera Informática y Desarrolladora Full Stack
-      </Typography>
+
+      <Box
+        display="flex"
+        flexDirection={{ xs: "column", sm: "row" }}
+        justifyContent="space-between"
+        alignItems={{ xs: "center", sm: "flex-start" }}
+        gap={4}
+        paddingRight={4}
+      >
+
+        <Box textAlign={{ xs: "center", sm: "left" }}>
+          <Typography variant="h2">
+            Rebeca Hernando Brecht
+          </Typography>
+          <Typography variant="h5" color="primary" fontWeight="bold">
+            Ingeniera Informática y Desarrolladora Full Stack
+          </Typography>
+        </Box>
+
+        <Avatar
+          alt="Rebeca Hernando"
+          src="/static/images/avatar.png"
+          sx={{
+            width: { xs: 150, sm: 200 },
+            height: { xs: 150, sm: 200 },
+            bgcolor: theme.palette.primary.main,
+            fontSize: { xs: 60, sm: 80 },
+            mt: { xs: 2, sm: 0 },
+          }}
+        />
+      </Box>
 
       <Grid container sx={{ p: "2em" }} spacing={2}>
         <Grid size={{ xs: 12, md: 4 }}>
