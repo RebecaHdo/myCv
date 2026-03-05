9c03572ba590aeac274ba2872c1540845e794846
unknown <rbkhb98@gmail.com>
2026-03-05 15:24:46 +0100

    changed orgins corss and avatar position

--- chatBot/app/__init__.py
+++ chatBot/app/__init__.py
@@ -10,7 +10,7 @@ def create_app(testing=False):
 
     origins = [
         "http://localhost:5173",       
-        "https://rebecahdo.github.io/",
+        "https://rebecahdo.github.io",
 
     ]
     CORS(app, origins=origins)
--- front/src/components/cv/Header.tsx
+++ front/src/components/cv/Header.tsx
@@ -55,7 +55,7 @@ export default function Header() {
 
           <Avatar
             alt="Rebeca Hernando"
-            src="/static/images/avatar.png"
+            src="assets/avatar.png"
             sx={{
               width: { xs: 150, sm: 200 },
               height: { xs: 150, sm: 200 },
