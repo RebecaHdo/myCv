b162292c65ce3e0a2b51336dd9a2c0a58d905500
unknown <rbkhb98@gmail.com>
2026-03-18 16:51:18 +0100

    add relation recipe to ingredients

--- recipeApi/recipes/serializers/recipe_serializer.py
+++ recipeApi/recipes/serializers/recipe_serializer.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
 from recipes.models.recipe import Recipe
+from recipes.models.recipe_ingredient import RecipeIngredient
+
+
+class RecipeIngredientInputSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    quantity = serializers.FloatField()
+    unit = serializers.CharField()
 
 
 class RecipeSerializer(serializers.ModelSerializer):
+
+    ingredients = RecipeIngredientInputSerializer(many=True, write_only=True)
 
     class Meta:
         model = Recipe
@@ -15,4 +24,21 @@ class RecipeSerializer(serializers.ModelSerializer):
             "servings",
             "difficulty",
             "author",
+            "ingredients"
         ]
+
+    def create(self, validated_data):
+
+        ingredients_data = validated_data.pop("ingredients")
+
+        recipe = Recipe.objects.create(**validated_data)
+
+        for item in ingredients_data:
+            RecipeIngredient.objects.create(
+                recipe=recipe,
+                ingredient_id=item["id"],
+                quantity=item["quantity"],
+                unit=item["unit"]
+            )
+
+        return recipe
