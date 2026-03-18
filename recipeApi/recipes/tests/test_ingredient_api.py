2d5565c9646d16052571ddc1684a9c27d3031fe8
unknown <rbkhb98@gmail.com>
2026-03-18 16:22:34 +0100

    fix ingredient tests to pass it without reset BD

--- recipeApi/recipes/tests/test_ingredient_api.py
+++ recipeApi/recipes/tests/test_ingredient_api.py
@@ -1,6 +1,7 @@
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
+from datetime import datetime
 
 from recipes.models.ingredient import Ingredient
 
@@ -10,36 +11,69 @@ class IngredientAPITest(APITestCase):
     def test_create_ingredient(self):
         url = reverse("ingredient-list")
 
+        ingredient_name = f"Prueba-Tomate-{datetime.now()}"
+
         data = {
-            "name": f"Tomate-{uuid.uuid4()}"
+            "name": ingredient_name
         }
 
         response = self.client.post(url, data)
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Ingredient.objects.count(), 1)
+
+        ingredients_tomato = Ingredient.objects.filter(name=ingredient_name)
+
+        self.assertEqual(len(ingredients_tomato), 1)
 
 
     def test_update_ingredient(self):
-        ingredient = Ingredient.objects.create(name="Tomate")
+
+        url = reverse("ingredient-list")
+        
+        ingredient_name = f"Prueba-Tomate-{datetime.now()}"
+
+        data = {
+            "name": ingredient_name
+        }
+
+        response = self.client.post(url, data)
+
+        ingredient = Ingredient.objects.get(name=ingredient_name)
 
         url = reverse("ingredient-detail", args=[ingredient.id])
 
-        data = {"name": "Tomate Cherry"}
+        ingredient_name_updated = ingredient_name+"Cherry"
+        data = {"name": ingredient_name_updated}
 
         response = self.client.patch(url, data)
 
         ingredient.refresh_from_db()
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(ingredient.name, "Tomate Cherry")
+        self.assertEqual(ingredient.name, ingredient_name_updated)
 
     def test_delete_ingredient(self):
-        ingredient = Ingredient.objects.create(name="Tomate")
 
-        url = reverse("ingredient-detail", args=[ingredient.id])
+        url = reverse("ingredient-list")
+
+        ingredient_name = f"Prueba-Tomate-{datetime.now()}"
+
+        data = {
+            "name": ingredient_name
+        }
+
+        response = self.client.post(url, data)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        ingredient_id = response.data["id"]
+
+        url = reverse("ingredient-detail", args=[ingredient_id])
 
         response = self.client.delete(url)
 
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        self.assertEqual(Ingredient.objects.count(), 0)
+
+        ingredients = Ingredient.objects.filter(name=ingredient_name)
+
+        self.assertEqual(ingredients.count(), 0)
